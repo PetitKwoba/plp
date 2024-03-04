84ebe6235c052c89e47905725d6963d49f3da88a
Emmanuel Kwoba <kwobapetit@gmail.com>
2024-03-04 14:04:29 +0300

    week one assignment with variables that take three inputs; name, age and location ten outputs a personalized message

---
+++ python/weekOne/user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+location= input("Enter Your location: ")
+
+print("Hello " + name + ", you are " + age + " and you live in " + location);
+
